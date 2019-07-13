34e2d4dc32fceb5a9bc0c50b60cec840383405a7
thejihongjin <46304180+thejihongjin@users.noreply.github.com>
2019-07-13 16:03:07 -0700

    added boilerplate from 07-Ins_Mern example

---
+++ create-react-express/client/build/precache-manifest.004ec478e8629202221d2bf315d96160.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4b6555ca2ec798abbd75",
+    "url": "/static/css/main.62e0474a.chunk.css"
+  },
+  {
+    "revision": "4b6555ca2ec798abbd75",
+    "url": "/static/js/main.f8078d09.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "495d93cc2c05774c4e88882137125113",
+    "url": "/static/media/logo.495d93cc.svg"
+  },
+  {
+    "revision": "fdc63a66102ace32736e26eb9c9abde6",
+    "url": "/index.html"
+  }
+];
