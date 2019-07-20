a299774a9d12442858c92c138ec9278f81233015
thejihongjin <46304180+thejihongjin@users.noreply.github.com>
2019-07-20 11:27:07 -0700

    added mongoose npm package and added code to connect to mongoDB database in server.js

--- create-react-express/server.js
+++ create-react-express/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mongoose = require("mongoose");
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
@@ -10,6 +11,9 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/googlebooks";
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
 // Define API routes here
 
